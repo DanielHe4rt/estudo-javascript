cae13f261de8c00c3a653e795d9d993104a50696
Daniel Reis <daniel_o_amigo@hotmail.com>
2018-11-28 21:39:44 -0200

    Finalização do JS Drum Kit

---
+++ 01-drumkit/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('keydown',function(e){
+    let button = document.querySelector(`button[data-key='${e.keyCode}']`)
+    if(!button){
+        console.log("botão errado, amigo.")
+        return false;
+    }
+    button.classList.add('clicked')
+    let action = button.querySelector('div').textContent.toLowerCase()
+    console.log(`Botão: ${e.key} | Codigo: ${e.keyCode}`)
+    let audio = new Audio(`sounds/${action}.wav`);
+
+    console.log(audio)
+    audio.play();
+})
+
+document.addEventListener('keyup',function(e){
+    let button = document.querySelector(`button[data-key='${e.keyCode}']`)
+    if(!button){
+        console.log("botão errado, amigo.")
+        return false;
+    }
+    button.classList.remove('clicked');
+})
